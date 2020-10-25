941faf14869a5770e96015c879765561f4c1a6d2
Tomas Samuel Lang <langtom997@icloud.com>
2020-10-25 16:46:45 +0100

    added material design and angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,17 @@ import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatMenuModule,
+  MatIconModule,
+  MatToolbarModule,
+  MatGridListModule,
+} from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -20,7 +31,16 @@ import { FooterComponent } from './components/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatMenuModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatGridListModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
